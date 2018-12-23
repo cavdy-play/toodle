411d04f9f2bfac274dade798f27b3cb364309395
Cavdy <cavdyofficials@gmail.com>
2018-12-23 11:41:07 +0100

    added expressjs and handelbars

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 
 const app = express();
+
+// Handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Index Route 
+app.get('/', (req, res) => {
+    const title = 'Welcome';
+    res.render('index', {
+        title: title
+    });
+});
+
+// About Route
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 const port = 5000;
 
