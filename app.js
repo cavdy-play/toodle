5855b41c373a2c5fd42ad4ef421cd8d5eb24a6da
Cavdy <cavdyofficials@gmail.com>
2018-12-24 12:39:14 +0100

    added connect flash dependencies

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const exphbs  = require('express-handlebars');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -24,6 +27,26 @@ app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Method Override Middleware
+app.use(methodOverride('_method'));
+
+// Session Middleware
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(flash());
+
+// Global variables
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
 // Index Route 
 app.get('/', (req, res) => {
     const title = 'Welcome';
@@ -37,9 +60,33 @@ app.get('/about', (req, res) => {
     res.render('about');
 });
 
+// Todo index page
+app.get('/todo', (req, res) => {
+    Todo.find({
+
+    }).sort({
+        date: 'desc'
+    }).then(todos => {
+        res.render('todo/index', {
+            todos: todos
+        });
+    }).catch(err => console.log(err));
+});
+
 // Add Todo
 app.get('/todo/add', (req, res) => {
     res.render('todo/add');
+});
+
+// Edit Todo
+app.get('/todo/edit/:id', (req, res) => {
+    Todo.findOne({
+        _id: req.params.id
+    }).then(todo => {
+        res.render('todo/edit', {
+            todo: todo
+        });
+    }).catch(err => console.log(err));
 });
 
 // Process Form
@@ -63,8 +110,43 @@ app.post('/todo', (req, res) => {
             tag: req.body.tag
         });
     } else {
-        res.send('passed');
+        const newTodo = {
+            title: req.body.title,
+            details: req.body.details,
+            tag: req.body.tag
+        }
+        new Todo(newTodo).save().then(todo => {
+            req.flash('success_msg', 'Todo list added');
+            res.redirect('/todo');
+        }).catch(err => console.log(err));
     }
+});
+
+// Edit form
+app.put('/todo/:id', (req, res) => {
+    Todo.findOne({
+        _id: req.params.id
+    }).then(todo => {
+        // New 
+        todo.title = req.body.title;
+        todo.details = req.body.details;
+        todo.tag = req.body.tag;
+
+        todo.save().then(todo => {
+            req.flash('success_msg', 'Todo list edited');
+            res.redirect('/todo');
+        }).catch(err => console.log(err));
+    }).catch(err => console.log(err));
+});
+
+// Delete todo
+app.delete('/todo/:id', (req, res) => {
+    Todo.deleteOne({
+        _id: req.params.id
+    }).then(() => {
+        req.flash('success_msg', 'Todo list deleted');
+        res.redirect('/todo');
+    }).catch(err => console.log(err));
 });
 
 const port = 5000;
